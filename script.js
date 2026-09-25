b55696dd86532a3e2ad146e52332d91940807317
JOAO BATISTA STURZA D`AGOSTINI <87687410068@senacrs.edu.br>
2026-09-25 08:17:33 -0300

    new

--- script.js
+++ script.js
@@ -1,37 +1,66 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
-
-// x, y - Posicionar o obejeto
-// w, h - Definir o tamanho do personagem
-// vx - Define a velocidade Horizontal
-
-const player = {x: 40, y: 160, w: 128, h: 128, vx: 120};
-
-let last = 0; //Marca a posição do quadro anterior.
-
+ 
+// x, y - Posicionar o objeto
+// w, h - Definir tamanho do personagem
+// vx - Define velocidade horizontal
+ 
+//personagem e onde estara/dimensoes:
+const player = {x: 40, y: 160, w: 32, h: 32, vx: 120, vy: 120}
+ 
+//Guarda do tempo do frame anterior:
+let last = 0;
+ 
+//funcao que atualiza frames, a posição do jogador
 function update(dt){
+    //dt é o tempo que passou entre um quadro e outro,
+    //fazendo que o movimento do jogo aconteça baseado no tempo e não na quantidade de frames
+    // se fosse por frames andaria mais rapido em um pc que faz mais frames por segundo (fps)
     player.x += player.vx * dt;
-    // Bater na parede esquerda ou direita? Inverte o sinal do vx
-    if (player.x < 0 || player.x + player.w > canvas.width){
+    //Bater na parede esquerda ou direita = Inverter o sinal do vx
+    player.y += player.vy * dt; //Bater na parede cima ou debaixo = Inverter o sinal do vy
+    if (player.x < 0 || player.x + player.w > canvas.width){ // || = ou
         player.vx *= -1;
+        } // * -1 = inverter sinal
+        if (player.y < 0 || player.y + player.h > canvas.height){
+        player.vy *= -1;
+        }
 }
-}
-const pcIcon = new Image();
-pcIcon.src = "Untitled design.png";
+ 
+//Função desenha o personagem, recriando ele a cada movimento
 function draw(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-ctx.drawImage(pcIcon, player.x, player.y, player.w, player.h);
-    ctx.fillText("O DeltaTime - dt independe da taxa de quadros", 12, 20);
+    ctx.clearRect(0, 0, canvas.width, canvas.height); //zera valores quando é redesenhado
+    ctx.fillStyle = "#4ade80"; //cor personagem
+    //ctx.fillRect(player.x, player.y, player.h, player.w); //passa as formas definidas antes RETANGULO
+   
+    // ctx.fillStyle = "#fff";
+    //ctx.fillRect(player.x, player.y, player.h, player.w);  NA COR BRANCA
+ 
+    ctx.beginPath(); //começa uma nova forma
+    ctx.arc(player.x + player.w /2, player.y + player.h /2, player.w /2, 0, Math.PI * 2); // ctx.arc(x, y, raio, inicio, fim) ele faz o circulo
+    // x = onde fica o centro do circulo na horizontal, player.x = começo do jogador e player.w / 2 = metade da largura, 16px depois do inicio(x)
+    // y = onde fica o centro do circulo na vertical, player.y = começo do jogador e player.h / 2 = metade da largura, 16px depois do inicio(y)
+    // raio = distancia entre centro e a borda
+    // inicio = 0 pois comeca no angulo 0
+    // fim = Math.PI * 2 pois 180 graus = pi, pi * 2 = 360 graus
+    ctx.fill();
+ 
+    ctx.fillText("O DeltaTime - dt independe da taxa de quadros", 12, 20); //texto dentro do quadro
+ 
 }
-
+ 
+//funcao de loop, taxa de atualizacao, ts= taxa segundos
 function loop(ts){
-    if (!last) last = ts;
-
-    const dt = (ts - last) / 1000; // DeltaTime em segundos
+    if(!last) last = ts; // ! = validacao, negação logica
+    const dt = Math.min(0.05, (ts - last)/1000); //1000ms = 1seg. mathmin = pega o menor valor dos numeros, compara 0,05 com (ts - last) / 1000
+    last = ts;
     update(dt);
     draw();
-    last = ts;
     requestAnimationFrame(loop);
 }
-
-requestAnimationFrame(loop); // Inicia o loop do jogo
+ 
+requestAnimationFrame(loop); // de fora executa o primeiro disparo do personagem
+ 
+// Eu uso dt porque ele faz o movimento depender do tempo,
+// deixando a velocidade independente da taxa de quadros
+ 
